c1238f66dcf5e06970a01077cfe63ace22878693
SomethingOnu <rogatelice@protonmail.com>
2021-07-13 00:34:25 +0900

    年齢が自動で更新される機能を追加

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const age = document.getElementById('age');
 const notice = document.getElementById('notice');
 const works = document.getElementById('profile-4');
+
+const date_birth = new Date("2002-07-11T15:00:00.000Z");
+const date_now = new Date();
+// 日本時刻はUTC+9なので設定したい時刻から9時間を引く
+
+const unix_diff = Math.floor((date_now - date_birth) / 1000);
+const leap_day = Math.floor((date_now.getFullYear() - date_birth.getFullYear() + date_birth.getFullYear() % 2000) / 4);
+const age_year = Math.floor((unix_diff / (60 * 60 * 24) - leap_day) / 365);
+
+age.innerHTML = age_year;
 
 const onLoad = async () => {
     const notice_data = await fetch('notice.json').then(resp => resp.json());
